fb4d6c2c07161488c33ea29ba70b8f5862d92e97
Burak Demirkaya <buraqdmrky@gmail.com>
2025-02-17 14:37:47 +0300

    Fixed Database Deletion Error

--- stonks/app.py
+++ stonks/app.py
@@ -30,7 +30,6 @@ def start_database(app: Flask):
     database.init_app(app)
 
     with app.app_context():
-        database.drop_all()
         database.create_all()
     
     migrate = Migrate(app, database)
